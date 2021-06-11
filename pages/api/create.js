724516d17a2f1c9e2362938b06df46554b8e85b8
jaredgorski <jared.gorski@liferay.cloud>
2021-06-11 11:42:58 -0700

    Add wip create endpoint

---
+++ pages/api/create.js
@@ -0,0 +1,9 @@
+export const create = async () => {
+  if (req.method === 'POST') {
+    const  = req.body;
+    // Process a POST request
+    res.status(200);
+  } else {
+    res.status(404);
+  }
+};
